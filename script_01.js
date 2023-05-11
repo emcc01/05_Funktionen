95f39ad69025bd64f7234324143f459f8646d865
Eva <e.moritz.comcave@gmail.com>
2023-05-11 12:41:34 +0200

    geteilt in fkt

--- script_01.js
+++ script_01.js
@@ -34,12 +34,71 @@ function ausgabeNamen()
 /***** Funktionen 02b *****/
 // 2b. Parametrisierung + Datenübergabe von AUSSEN
 
-ausgabeNamenParam("Asad"); // Argument (Daten für Parameter)
-ausgabeNamenParam("Roxana");
+// ausgabeNamenParam("Asad"); // Argument (Daten für Parameter)
+// ausgabeNamenParam("Roxana");
 
-//Argumente sind Daten für Parameter
+// //Argumente sind Daten für Parameter
 
-function ausgabeNamenParam(firstName)  //Parameter, von aussen erreichbar
- {
-    console.log("Hallo " + firstName + "!");
+// function ausgabeNamenParam(firstName)  //Parameter, von aussen erreichbar
+//  {
+//     console.log("Hallo " + firstName + "!");
+// }
+
+
+/***** Funktionen 02c *****/
+// 2c. Mehrere Parameter
+
+// ausgabeNamenParams("Dave", "Batista");
+// ausgabeNamenParams("Jamshid", "Rahmanzani");
+// ausgabeNamenParams(prompt("Vorname?"), prompt("Nachname?"));
+
+// function ausgabeNamenParams(firstName, familyName) 
+// {
+//     console.log("Hallo " + firstName + " " + familyName + "!");
+// }
+
+
+/***** Funktionen 03a *****/
+// 03a. Vorbereitung
+// Postulat: one function = one job (uncle Bob)
+// SRP single responsibility principle
+
+// ausgabeNamenParams2("Dave", "Batista");
+
+function ausgabeNamenParams2(firstName, familyName) 
+{
+   // console.log("Hallo " + firstName + " " + familyName + "!");
+    // 1.Funktionalität: string composing,
+    const GAP = " ";                    //Grossschrift: Stringkonstanten 
+    let outputStr = "Hallo" + GAP + firstName + GAP + familyName + "!";
+
+
+    // 2. String output
+    console.log(outputStr);
+
+
+    // refactoring: code umbauen
+}
+
+/***** Funktionen 03b *****/
+// Trennen der Funktionalitäten in Funktionen| return
+
+output (getString("Maxine", "Mützerich"));
+output(getString(prompt("Vorname?"), prompt("Nachname?")));
+
+function getString(firstName, familyName) 
+{
+    const GAP = " ";                    
+    let outputStr = "Hallo" + GAP + firstName + GAP + familyName + "!";
+    return outputStr;  // return sendet Daten an den Call zurück
+    console.log(outputStr); // return bricht die Fkt ab
+}
+// Input -> Parameter, return -> Output
+
+// 2. Funktinalität: string output
+// output("Hi");
+// output(true);
+function output(outputData)
+{
+    console.log(outputData);
 }
